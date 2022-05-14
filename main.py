63a15fbd93bd30222202afc4c1115853ecc7ab61
for-the-zero <ourmicroid@outlook.com>
2022-05-14 16:54:26 +0800

    更新架构并修改bug

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ def go():
     q = ui.ask()
     q = cutwords(q)
     all_e = sm.get_all(q)
-    '''
-    #获取所有all_e的值，并el.debuglist(all_e的每一项)再el.inone(all_e的全部每一项)得到一个列表输出至answer列表中
-    for i in all_e:
-        all_e[i] = el.debuglist(all_e[i])
-    '''
     j = 0
     for i in all_e:
         if j == 0:
@@ -27,14 +22,4 @@ def go():
         else:
             answer = el.inone(answer,all_e[i])
         j += 1
-    '''
-    answer = all_e[0]
-    del all_e[0]
-    if len(all_e) != 0:
-        for i in all_e:
-            answer = el.inone(answer,all_e[i])
-    '''
     ui.show(q,answer)
-    #input()
-
-#go()
